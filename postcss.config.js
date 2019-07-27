4a0a0be69e71b21c4aa0b21f88bf634f0d3ff43f
Hector Signoriello <hector.signoriello@gmail.com>
2019-07-27 00:52:07 -0300

    install postcss-cssnext

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: [
+        // require('autoprefixer')({
+        //     grid:true
+        // }),
+        require('postcss-cssnext')({
+            feature:{
+                autoprefixer:{
+                    grid:true,
+                }
+            }
+        })
+    ]
+}
